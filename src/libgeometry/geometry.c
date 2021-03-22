3f6c7305c461cf753d0824e0d456be390331cf16
Vladimr Yuzov <vladimir.iuzow@yandex.ru>
2021-03-22 18:02:24 +0700

    Check build

--- src/libgeometry/geometry.c
+++ src/libgeometry/geometry.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 bool skip_space(int* circumflex_counter, char** first, char** second)
 {
     while (isspace(**first)) {
--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #define SIZE_OF_ARR 256
 
 struct Circle {
