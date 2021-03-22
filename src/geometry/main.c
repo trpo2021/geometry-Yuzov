2bd19487858a98faa5e38dadd196059794697f55
Vladimr Yuzov <vladimir.iuzow@yandex.ru>
2021-03-22 17:51:41 +0700

    Return main to its original form

--- src/geometry/main.c
+++ src/geometry/main.c
@@ -1,6 +1,6 @@
-#include ctype.h>
-#include libgeometry / geometry.h>
-#include <stdbool.h
+#include <ctype.h>
+#include <libgeometry/geometry.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
