2e1274270f96f6f700272e0b6d42e1ebb9c3d961
Vladimir <vladimir.iuzow@yandex.ru>
2021-04-28 20:47:46 +0700

    Change structure for testing

---
+++ test/tests.c
@@ -0,0 +1,17 @@
+#include <ctest.h>
+#include <libgeometry/geometry.h>
+#include <stdlib.h>
+
+CTEST(arithmetic_suite, simple_sum)
+{
+    // Given
+    const int a = 1;
+    const int b = 2;
+
+    // When
+    const int result = sum(a, b);
+
+    // Then
+    const int expected = 3;
+    ASSERT_EQUAL(expected, result);
+}
